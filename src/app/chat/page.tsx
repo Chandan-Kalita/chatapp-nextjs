1254a04752caddec8bd14339509dc6374595fa3f
Chandan Kalita <ck3196686@gmail.com>
2024-01-01 02:00:50 +0530

    added rematch

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,20 +1,33 @@
+'use client';
+import { Dispatch, RootState, store } from '@/src/store/store';
 import { ContactStact } from '../../components/ContactStact';
 import { TextArea } from '../../components/TextArea';
+import { Provider, createStoreHook, useDispatch, useSelector } from 'react-redux';
+
+
+function App() {
+  return (<div className="bg-gradient-to-b from-green-800  absolute top-0 left-0 h-[100%] w-[100%]">
+    <div className=' bg-white  relative h-[calc(100%-38px)] w-[calc(100%-38px)] top-[19px] m-auto flex border-2 border-black rounded-[10px] overflow-auto'>
+      <div className=" flex flex-col p-2 h-[100%] w-[80%] max-w-[350px] ">
+        <ContactStact name={"Chandan Kalita"} />
+        <ContactStact name={"Rupanjan"} />
+        <ContactStact name={"Parashar"} />
+        <ContactStact name={"Bhaskar"} />
+      </div>
+      <div className="relative h-[100%] w-[100%] bg-gray-900">
+        <TextArea />
+      </div>
+    </div>
+  </div>);
+}
+
 
 export default function Home() {
   return (
-    <div className="bg-gradient-to-b from-green-800  absolute top-0 left-0 h-[100%] w-[100%]">
-      <div className=' bg-white  relative h-[calc(100%-38px)] w-[calc(100%-38px)] top-[19px] m-auto flex border-2 border-black rounded-[10px] overflow-auto'>
-        <div className=" flex flex-col p-2 h-[100%] w-[80%] max-w-[350px] ">
-          <ContactStact name={"Chandan Kalita"} />
-          <ContactStact name={"Rupanjan"} />
-          <ContactStact name={"Parashar"} />
-          <ContactStact name={"Bhaskar"} />
-        </div>
-        <div className="relative h-[100%] w-[100%] bg-gray-900">
-          <TextArea />
-        </div>
-      </div>
-    </div>
+    <Provider store={store}>
+      <App></App>
+    </Provider>
   )
 }
+
+
--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -1,16 +1,23 @@
 import { FaceSmileIcon, PaperAirplaneIcon, PlusIcon } from '@heroicons/react/24/outline';
 import { Message } from './Message';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState, Dispatch } from '../store/store';
 
 export function TextArea() {
+  const countState = useSelector((state: RootState) => state.count);
+  const dispatch = useDispatch<Dispatch>();
+  function handleClick(){
+    dispatch.count.increment(1);
+  }
   return (
     <div className="absolute top-0 left-0 h-full w-full ">
       <div className=" h-[calc(100%-60px)] w-full" style={{ backgroundImage: "url(./bg.png)", }}>
         <Message text={"Hello"} sender={"me"} />
-        <Message text={"Hii"} sender={"other"} />
+        <Message text={countState.toString()} sender={"other"} />
       </div>
       <div className=" bg-green-600 h-[60px] w-full flex">
         <div className="w-[60px] flex justify-center items-center">
-          <PlusIcon className=' w-[30px] text-white' />
+          <PlusIcon className=' w-[30px] text-white' onClick={handleClick}/>
         </div>
         <div className="flex-grow-[1] p-2">
           <div className="bg-white flex h-full p-2">
